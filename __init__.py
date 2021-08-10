01ecab9289886b8dd993536f0c2da76a7998ff65
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-08-10 20:08:34 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+import os
+from pyrogram import Client
+
+API_ID = "2192067"
+API_HASH = "d2e0ba99f1b9cdb632b43633edb76f11"
+TOKEN = "1710612658:AAFLE-PZuJmN8bj4B4YX19peWvY5mjgpx7I"
+
+Bot = Client(':memory:', api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
