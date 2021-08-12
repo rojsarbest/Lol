25b5001d441fb4738e0ebb6aa3bef239b89cd739
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-08-12 11:06:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -214,10 +214,7 @@ else:
            audio_file = audio_file_weba
        # thumbnail
        title = s2tw(info_dict['title'][:40])    
-       thumbnail_file = f'thumb{title}.jpg'
-       prabhasha = s2tw(info_dict['thumbnails'][:0])
-       thumb = requests.get(prabhasha, allow_redirects=True)
-       open(thumbnail_file, 'wb').write(thumb.content)      #info_dict thumbnail
+       thumbnail_file = f'thumb{title}.jpg'      #info_dict thumbnail
        # info (s2tw)                                        
        webpage_url = info_dict['webpage_url']                     
        title = s2tw(info_dict['title'])
