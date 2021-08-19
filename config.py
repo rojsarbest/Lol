ff637ba2b8f38458149a2d960914a9800a6cb7df
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-08-20 02:20:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,28 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-
-    API_HASH = os.environ.get("API_HASH", "")
-
+    TG_BOT_TOKEN = "1710612658:AAEQHPxRC2mDZcZBkPKyd0i0JA9aofIK9nw"
+    APP_ID = "2192067"
+    API_HASH = "d2e0ba99f1b9cdb632b43633edb76f11"
     AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "")
-
     VIDEO_THUMBNAIL = os.environ.get("VIDEO_THUMBNAIL", "")
-
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+
+
+
+#import os
+
+
+#class Config(object):
+  #  TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+   # APP_ID = int(os.environ.get("APP_ID", 12345))
+
+   # API_HASH = os.environ.get("API_HASH", "")
+
+  #  AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "")
+
+  #  VIDEO_THUMBNAIL = os.environ.get("VIDEO_THUMBNAIL", "")
+
+  #  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+
