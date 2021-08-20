71834159c59ce53b400f96262214bb65701d7280
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-08-20 15:50:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = "1710612658:AAF9MbcpFHrJ-ZLP8XbrtC-U6emqp2Emaeo"
-    APP_ID = "2192067"
-    API_HASH = "d2e0ba99f1b9cdb632b43633edb76f11"
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    APP_ID = int(os.environ.get("APP_ID", 12345))
+    API_HASH = os.environ.get("API_HASH", "")
     AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "")
     VIDEO_THUMBNAIL = os.environ.get("VIDEO_THUMBNAIL", "")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
