fbcfabd5c95957480d42d02c14462e67aee315f9
Prabhasha <74059196+prabhasha-p@users.noreply.github.com>
2021-08-21 07:07:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os  # song 
 import wget
 import requests
 import aiohttp
@@ -150,87 +150,6 @@ async def help(client, message):
 select_img = "https://telegra.ph/file/b38318f5d3e2e5201db40.png"       
 #Download By Name 
 
-@bot.on_message (filters.private & ~filters.bot)
-async def song(client, message):
-    url = message.text
-    user = message.from_user.mention
-    rkp = await message.reply("Processing...")
-    search = SearchVideos(url, offset=1, mode="json", max_results=1)
-    test = search.result()
-    p = json.loads(test)
-    q = p.get("search_result")
-    capz = q[0]["title"]
-    cap = f"**Song Name ➠** `{capz}` \n**Requested For :** `{url}` \n**Requested By :** {user} \n**Uploaded By :** @Bot"
-    try:
-        url = q[0]["link"]
-    except BaseException:
-        return await rkp.edit("Failed to find that song.")
-    type = "audio"
-    if type == "audio":
-        opts = {
-            "format": "bestaudio",
-            "addmetadata": True,
-            "key": "FFmpegMetadata",
-            "writethumbnail": True,
-            "prefer_ffmpeg": True,
-            "geo_bypass": True,
-            "nocheckcertificate": True,
-            "postprocessors": [
-                {
-                    "key": "FFmpegExtractAudio",
-                    "preferredcodec": "mp3",
-                    "preferredquality": "320",
-                }
-            ],
-            "outtmpl": "%(id)s.mp3",
-            "quiet": True,
-            "logtostderr": False,
-        }
-        song = True
-    try:
-        await rkp.edit("Downloading...")
-        with YoutubeDL(opts) as rip:
-            rip_data = rip.extract_info(url)
-    except DownloadError as DE:
-        await rkp.edit(f"`{str(DE)}`")
-        return
-    except ContentTooShortError:
-        await rkp.edit("`The download content was too short.`")
-        return
-    except GeoRestrictedError:
-        await rkp.edit(
-            "`Video is not available from your geographic location due to geographic restrictions imposed by a website.`"
-        )
-        return
-    except MaxDownloadsReached:
-        await rkp.edit("`Max-downloads limit has been reached.`")
-        return
-    except PostProcessingError:
-        await rkp.edit("`There was an error during post processing.`")
-        return
-    except UnavailableVideoError:
-        await rkp.edit("`Media is not available in the requested format.`")
-        return
-    except XAttrMetadataError as XAME:
-        await rkp.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
-        return
-    except ExtractorError:
-        await rkp.edit("`There was an error during info extraction.`")
-        return
-    except Exception as e:
-        await rkp.edit(f"{str(type(e)): {str(e)}}")
-        return
-    time.time()
-    if song:
-        await rkp.edit("Uploading...") 
-        lel = await message.reply_audio(
-                 f"{rip_data['id']}.mp3",
-                 duration=int(rip_data["duration"]),
-                 title=str(rip_data["title"]),
-                 performer=str(rip_data["uploader"]),
-                 thumb=select_img,
-                 caption=cap)  
-        await rkp.delete()
 
 #cutted_from
 @bot.on_callback_query(filters.regex("^ytdl_audio$"))
